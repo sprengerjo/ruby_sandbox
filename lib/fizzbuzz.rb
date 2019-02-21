ad3f32a0af4145245dcb0c1801e2c71249751574
sprengerjo <jonas.sprenger@huf-group.com>
2019-02-21 10:16:26 +0100

    finalize fizzbuzz

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,16 @@
+
+def fizzbuzz(n)
+    if n % 15 == 0
+        'fizzbuzz'
+    elsif n % 3 == 0
+        'fizz'
+    elsif n % 5 == 0
+        'buzz'
+    else 
+        n.to_s
+    end
+end
+
+def fizzbuzz_upto(n)
+    (1..n).map { |n| fizzbuzz(n) }
+end
