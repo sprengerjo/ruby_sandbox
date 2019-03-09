2d67add7a50813e66fe3f6540ccc36a3bb2bff92
sprengerjo <jonas.sprenger@huf-group.com>
2019-03-09 19:56:32 +0100

    add twice linear kata

---
+++ lib/algorithms.rb
@@ -0,0 +1,23 @@
+
+class Algorithms
+    @cache = {0 => 1}
+
+    def self.twice_linear(n)
+        a = 1
+        while n > 0
+            a = knuth(a)
+            n = n - 1 
+        end
+        a
+    end
+
+    def self.knuth(a) 
+        if not @cache.key?(a)
+            @cache[a] = 1 + [
+                2 * knuth(((a - 1) / 2).floor), 
+                3 * knuth(((a - 1) / 3).floor)
+            ].min
+        end
+        @cache[a]
+    end
+end
