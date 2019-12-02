a1c33fa052fa650c13b315cd24083a305661510d
Jonas Sprenger <jonas.sprenger@invision.de>
2019-12-02 13:00:29 +0100

    Update Ruby version and add class to fizzbuzz solution

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,15 +1,18 @@
-def fizzbuzz(curr)
-  if (curr % 15).zero?
-    'fizzbuzz'
-  elsif (curr % 3).zero?
-    'fizz'
-  elsif (curr % 5).zero?
-    'buzz'
-  else
-    curr.to_s
-  end
-end
+class FizzBuzz
 
-def fizzbuzz_upto(uo_to)
-  (1..uo_to).map { |curr| fizzbuzz(curr) }
+  def parse_number(curr)
+    if (curr % 15).zero?
+      'fizzbuzz'
+    elsif (curr % 3).zero?
+      'fizz'
+    elsif (curr % 5).zero?
+      'buzz'
+    else
+      curr.to_s
+    end
+  end
+
+  def upto(uo_to)
+    (1..uo_to).map { |curr| parse_number(curr) }
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,24 +1,27 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
+
+  let(:fizzbuzz) { FizzBuzz.new }
+
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq('fizz')
+    expect(fizzbuzz.parse_number(3)).to eq('fizz')
   end
 
   it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq('buzz')
+    expect(fizzbuzz.parse_number(5)).to eq('buzz')
   end
 
   it 'returns "fizzbuzz" when passed 15' do
-    expect(fizzbuzz(15)).to eq('fizzbuzz')
+    expect(fizzbuzz.parse_number(15)).to eq('fizzbuzz')
   end
 
   it 'returns "1" when passed 1' do
-    expect(fizzbuzz(1)).to eq('1')
+    expect(fizzbuzz.parse_number(1)).to eq('1')
   end
 
   it 'returns fizzbuzz sequence ending with "fizzbuzz" when passed 15' do
-    expect(fizzbuzz_upto(15))
+    expect(fizzbuzz.upto(15))
       .to eq(%w[1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz])
   end
 end
