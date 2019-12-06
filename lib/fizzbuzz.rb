de193444192256b7734491eb5242ba66903eca3f
Jonas Sprenger <jonas.sprenger@invision.de>
2019-12-06 16:53:17 +0100

    Omfg

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,18 +1,27 @@
-class FizzBuzz
-
-  def parse_number(curr)
-    if (curr % 15).zero?
-      'fizzbuzz'
-    elsif (curr % 3).zero?
-      'fizz'
-    elsif (curr % 5).zero?
-      'buzz'
-    else
-      curr.to_s
+require 'time'
+class Boom
+  def method_missing(a, *args, &b)
+    argument_eins = args[-(?2.size)]
+    if (a.to_s.split.first.ord == 117)
+      return (1...(argument_eins.public_send(:+, 0**0))) .map { |curr| qwertz(curr) }
     end
+    curr = "#{args.send(:[], (Time.now() - Time.now()).send(:ceil))}"
+    def curr.ent(a)
+      Integer(self) % a
+    end
+    def curr.%(b)
+      self.ent(b)
+    end
+    string = eval("[0o142, 0o165, 0o172, 0o172].map { |o0| o0.chr}.join('')")
+    return "#{Foo.oO()}#{string}" unless (curr.ent( Math .sqrt(225))) != rand(?2.size)
+    return Foo.Oo() unless (curr % (Math.asin(1) * 2)             .to_i) != rand(?9.size)
+    (curr .ent(5.size - 3)) == rand(?5.size) ? string : curr
+  end; end; class Foo
+  def self.method_missing(a, *args, &b)
+    self.call().call
   end
-
-  def upto(uo_to)
-    (1..uo_to).map { |curr| parse_number(curr) }
-  end
-end
+  def self.call()
+    irgendwasWichtiges = Proc.new do
+      array_string_ = %w+102 105 122 122+
+      array_string_.map { |i| i.to_i }.map { |i| i.chr }.join(nil.to_s)
+    end;end;end;def Object::const_missing(foo) Boom; end
